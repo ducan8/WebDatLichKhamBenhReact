1d72deeb1529817002957b65b168e2282c458f89
ducan8 <ducan9c@gmail.com>
2023-06-21 10:52:26 +0700

    hoàn thiện chức năng đặt lịch

--- src/services/userService.js
+++ src/services/userService.js
@@ -64,9 +64,13 @@ const  postPatientBookAppointment = (data) => {
     return axios.post(`/api/patient-book-appointment`, data)
 }
 
+const  postVerifyBookAppointment = (data) => {
+    return axios.post(`/api/verify-book-appointment`, data)
+}
+
 export { handleLoginApi, getAllUsers, createNewUserService, 
     deleteUserService, editUserService, getAllcodeService, 
     getTopDoctorHomeService, getAllDoctors, saveDetailDoctorService,
     getDetailInfoDoctor, saveBulkScheduleDoctor, getScheduleDoctorByDate,
     getExtraInforDoctorById, getProfileDoctorById,
-    postPatientBookAppointment };
+    postPatientBookAppointment, postVerifyBookAppointment };
