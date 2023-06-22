3710e45ee45739c3b5c59e38991efef40736cd62
ducan8 <ducan9c@gmail.com>
2023-06-22 17:40:47 +0700

    hoan thien  chi tiet phong kham

--- src/services/userService.js
+++ src/services/userService.js
@@ -80,10 +80,14 @@ const getAllDetailSpecialtyById = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`)
 }
 
+const createNewClinic = (data) => {
+    return axios.post(`/api/create-new-clinic`, data)
+}
+
 export { handleLoginApi, getAllUsers, createNewUserService, 
     deleteUserService, editUserService, getAllcodeService, 
     getTopDoctorHomeService, getAllDoctors, saveDetailDoctorService,
     getDetailInfoDoctor, saveBulkScheduleDoctor, getScheduleDoctorByDate,
     getExtraInforDoctorById, getProfileDoctorById,
     postPatientBookAppointment, postVerifyBookAppointment,
-    createNewSpecialty, getAllSpecialty, getAllDetailSpecialtyById };
+    createNewSpecialty, getAllSpecialty, getAllDetailSpecialtyById, createNewClinic };
