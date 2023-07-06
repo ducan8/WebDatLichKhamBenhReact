5ea50238c82c756ffb71a08e6b42d33ccbe0047c
ducan8 <ducan9c@gmail.com>
2023-07-06 19:26:00 +0700

    update messenger

--- src/containers/Patient/SocialPlugin/Chatbot.js
+++ src/containers/Patient/SocialPlugin/Chatbot.js
@@ -1,12 +1,14 @@
-// "use client";
-import { FacebookProvider, CustomChat } from "react-facebook";
+"use client";
+import React, { Component} from 'react';
+import { FacebookProvider, CustomChat } from 'react-facebook';
 
-const Chatbot = () => {
+export default class Chatbot extends Component {
+  render() {
     return (
-        <FacebookProvider appId="1565948963808728" chatSupport>
-            <CustomChat pageId="1565948963808728" minimized={true} />
-        </FacebookProvider>
-    )
+      <FacebookProvider appId="1565948963808728" chatSupport>
+        <CustomChat pageId="1565948963808728" minimized={true}/>
+      </FacebookProvider>    
+    );
+  }
 }
 
-export default Chatbot;
