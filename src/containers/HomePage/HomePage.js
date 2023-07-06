c53fdc7ee9e91c49ee00aeb0fd5ccc8857c9a9e7
ducan8 <ducan9c@gmail.com>
2023-07-06 20:43:17 +0700

    update messenger

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -7,7 +7,7 @@ import OutstandingDoctor from './Section/OutstandingDoctor';
 import HanBook from './Section/HandBook';
 import About from './Section/About';
 import HomeFooter from './Section/HomeFooter';
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 import './HomePage.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -26,8 +26,8 @@ class HomePage extends Component {
             // nextArrow: <SampleNextArrow />,
             // prevArrow: <SamplePrevArrow />
         };
-        let currentURL = +process.env.REACT_APP_IS_LOCALHOST === 1 ?
-            "https://chatbot-heathcare-775548f1d48a.herokuapp.com/" : window.location.href;
+        // let currentURL = +process.env.REACT_APP_IS_LOCALHOST === 1 ?
+        //     "https://chatbot-heathcare-775548f1d48a.herokuapp.com/" : window.location.href;
 
         return (
             <React.Fragment>
@@ -38,6 +38,11 @@ class HomePage extends Component {
                 <HanBook settings={settings} />
                 <About />
                 <HomeFooter />
+                <MessengerCustomerChat
+                    pageId="102601946221028"
+                    appId="1565948963808728"
+                    // htmlRef="<REF_STRING>"
+                />
                 {/* <Chatbot dataHref={currentURL} /> */}
             </React.Fragment>
         );
