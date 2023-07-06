6a083a2a40113c7e0e9ffa83070777a5c87ff6ea
ducan8 <ducan9c@gmail.com>
2023-07-06 19:37:26 +0700

    update messenger

--- src/containers/Patient/SocialPlugin/Chatbot.js
+++ src/containers/Patient/SocialPlugin/Chatbot.js
@@ -1,14 +1,79 @@
-"use client";
-import React, { Component} from 'react';
-import { FacebookProvider, CustomChat } from 'react-facebook';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { LANGUAGES } from '../../../utils';
 
-export default class Chatbot extends Component {
-  render() {
-    return (
-      <FacebookProvider appId="1565948963808728" chatSupport>
-        <CustomChat pageId="1565948963808728" minimized={true}/>
-      </FacebookProvider>    
-    );
-  }
+class Chatbot extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    initFacebookSDK() {
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "PAGE-ID");
+        chatbox.setAttribute("attribution", "biz_inbox");
+        if (window.FB) {
+            window.FB.XFBML.parse();
+        }
+
+        let { language } = this.props;
+        let locale = language === LANGUAGES.VI ? 'vi_VN' : 'en_US'
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: process.env.REACT_APP_FACEBOOK_APP_ID,
+                cookie: true,  // enable cookies to allow the server to access
+                // the session
+                xfbml: true,  // parse social plugins on this page
+                version: 'v2.5' // use version 2.1
+            });
+        };
+        // Load the SDK asynchronously
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = `//connect.facebook.net/${locale}/sdk.js`;
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
+
+    componentDidMount() {
+        this.initFacebookSDK();
+    }
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+    }
+
+    render() {
+
+        return (
+            <>
+                <div id="fb-root"></div>
+
+                <div id="fb-customer-chat" class="fb-customerchat">
+
+                </div>
+
+            </>
+        )
+    }
 }
 
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chatbot);
